d92f3b7d605017f73035f2ebf11e381100e4e197
Ruben Martinez Jr <ruben.martinez93@gmail.com>
2016-01-13 03:36:27 -0800

    Searching built in

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -6,9 +6,18 @@ TodoList = React.createClass({
     return {};
   },
   getMeteorData() {
+    //default params for retriving list of todos
+    const incompleteTodosParams = { checked: false, deleted: undefined };
+    const completeTodosParams   = { checked: true, deleted: undefined };
+    
+    //if we're confucting a search, also filter by regex matches against text
+    if(this.state.query) {
+      incompleteTodosParams.text = completeTodosParams.text = { $regex : ".*"+this.state.query+".*", '$options' : 'i' };
+    }
+    
     return {
-      incompleteTodos: Todos.find({ checked: false, deleted: undefined }, {sort: {createdAt: -1}}).fetch(),
-      completeTodos:   Todos.find({ checked: true,  deleted: undefined }, {sort: {updatedAt: -1}}).fetch()
+      incompleteTodos: Todos.find(incompleteTodosParams, {sort: {createdAt: -1}}).fetch(),
+      completeTodos:   Todos.find(completeTodosParams,   {sort: {updatedAt: -1}}).fetch()
     };
   },
   componentDidMount() {
@@ -40,6 +49,16 @@ TodoList = React.createClass({
     //we only want to undo one item
     this.setState({ justDeleted: undefined });
   },
+  onSearch() {
+    //clean up the query
+    let query = ReactDOM.findDOMNode(this.refs.query).value.trim();
+    query = query.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    
+    //update our state to search
+    this.setState({
+      query: query
+    });
+  },
   render() {
     let notification; //show notification for undoing
     if(this.state.justDeleted) {
@@ -51,6 +70,9 @@ TodoList = React.createClass({
         <header>
           <h2>Todos</h2>
         </header>
+        <section>
+          <input className="form-control" placeholder="Search..." ref="query" onChange={this.onSearch} />
+        </section>
         <ul id="sortable-incomplete" className="todoList list-group">
           {this.renderIncompleteTodos()}
         </ul>
