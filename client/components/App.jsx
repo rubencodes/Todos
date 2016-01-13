2c35d94bcf62ca63482c29637f037cda1ec98e0d
Ruben Martinez Jr <ruben.martinez93@gmail.com>
2016-01-12 23:03:53 -0800

    Init Todos app

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+App = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+      </div>
+    );
+  }
+});
